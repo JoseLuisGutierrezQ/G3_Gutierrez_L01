3cfff163aab368b2298a26151f33fb3172229918
JoseLuisGutierrezQ <101904595+JoseLuisGutierrezQ@users.noreply.github.com>
2025-03-18 16:34:41 -0500

    Add files via upload
    creacion de la clase coordenada

---
+++ grupo33/src/Coordenada.java
@@ -0,0 +1,36 @@
+import java.util.*;
+ public class Coordenada{
+    private double x;
+    private double y;
+    
+    public Coordenada (){
+
+    }
+    public Coordenada (doble x, doble y){
+
+    }
+    public Coordenada (Coordenada c){
+
+    }
+    void setX(double x){
+
+    }
+    void setY(double y){
+
+    }
+    double getX(){
+
+    }
+    double getY(){
+
+    }
+    doble distancia(Coordenada c){
+
+    }
+    static double distancia(Coordenada c1, Coordenada c2){
+
+    }
+    String toString(){
+        
+
+}
